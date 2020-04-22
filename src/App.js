8b8dddf3e42e4d773ded663b1d1401bdc69a2465
burhan_maseel <burhanmaseel@gmail.com>
2020-04-22 23:28:04 +0500

    Video 20: Using useState hook for state management

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi this is my first react app</h1>
-      </div>
-    );
-  }
-}
+const app = (props) => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      {
+        name: "Max",
+        age: "16"
+      },
+      {
+        name: "Manu",
+        age: "14"
+      },
+      {
+        name: "John",
+        age: "12"
+      },
+    ],
+    otherState: "Some other value"
+  });
 
-export default App;
+  const [ otherState, setOtherState] = useState('some other value');
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: "Maximilian", age: "16" },
+        { name: "Manu", age: "14" },
+        { name: "Johnathan", age: "27" }
+      ]
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi this is my first react app</h1>
+      <p>This is really working</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My hobbies: Badminton
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
+
+export default app;
